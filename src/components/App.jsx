39e3a323b71c0c5021cb542fa970a8e4dd64b811
jstone <jstonedev521@gmail.com>
2022-09-28 19:53:41 -0400

    create routes & basic file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <div>
+    <CssBaseline />
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <h1>Movies</h1>
+        </Route>
+        <Route path="/movie/:id">
+          <h1>Movie Information</h1>
+        </Route>
+        <Route path="/actors/:id">
+          <h1>Actors</h1>
+        </Route>
+        <Route path="/profile/:id">
+          <h1>Profile</h1>
+        </Route>
+      </Switch>
+    </main>
+  </div>
+);
 
 export default App;
