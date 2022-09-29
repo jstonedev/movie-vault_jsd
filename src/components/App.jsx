2924145c5c2f49ab34ffbfe4f276004ec2f17a4b
jstone <jstonedev521@gmail.com>
2022-09-28 21:20:13 -0400

    App styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,36 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 
+import useStyles from './styles';
+
 import { Actors, MovieInformation, Movies, Navbar, Profile } from './';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <Navbar />
-    <main>
-      <Switch>
-        <Route exact path="/">
-          <Movies />
-        </Route>
-        <Route path="/movie/:id">
-          <MovieInformation />
-        </Route>
-        <Route path="/actors/:id">
-          <Actors />
-        </Route>
-        <Route path="/profile/:id">
-          <Profile />
-        </Route>
-      </Switch>
-    </main>
-  </div>
-);
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
